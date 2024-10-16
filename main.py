772c284caa90709a02086926986aa81753302283
Vince Swu <vinceswu88@gmail.com>
2024-10-16 23:20:15 +0530

    chunks

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 from shader_program import ShaderProgram
 from scene import Scene
 from player import Player
+from textures import Textures
 
 class VoxelEngine:
     def __init__(self):
@@ -16,7 +17,6 @@ class VoxelEngine:
 
         pg.display.set_mode(WIN_RES, flags=pg.OPENGL | pg.DOUBLEBUF)
         self.ctx = mgl.create_context()
-
         self.ctx.enable(flags=mgl.DEPTH_TEST | mgl.CULL_FACE | mgl.BLEND)
         self.ctx.gc_mode = 'auto'
 
@@ -31,6 +31,7 @@ class VoxelEngine:
         self.on_init()
 
     def on_init(self):
+        self.textures = Textures(self)
         self.player = Player(self)
         self.shader_program = ShaderProgram(self)
         self.scene = Scene(self)
@@ -43,6 +44,7 @@ class VoxelEngine:
         self.delta_time = self.clock.tick()
         self.time = pg.time.get_ticks() * 0.001
         pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
+        print(self.clock.get_fps())
 
     def render(self):
         self.ctx.clear(color=BG_COLOR)
