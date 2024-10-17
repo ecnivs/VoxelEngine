d13ca8bb31686883615098660f0001b0e4787328
Vince Swu <vinceswu88@gmail.com>
2024-10-17 14:36:09 +0530

    update

--- frustum.py
+++ frustum.py
@@ -1,13 +1,10 @@
 from settings import *
 
-
 class Frustum:
     def __init__(self, camera):
         self.cam: Camera = camera
-
         self.factor_y = 1.0 / math.cos(half_y := V_FOV * 0.5)
         self.tan_y = math.tan(half_y)
-
         self.factor_x = 1.0 / math.cos(half_x := H_FOV * 0.5)
         self.tan_x = math.tan(half_x)
 
@@ -31,5 +28,4 @@ class Frustum:
         dist = self.factor_x * CHUNK_SPHERE_RADIUS + sz * self.tan_x
         if not (-dist <= sx <= dist):
             return False
-
         return True
